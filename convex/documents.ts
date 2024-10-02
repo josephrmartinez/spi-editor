63f24c3ace732dcf6b99abb6ed245ed30782e33b
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-10-02 16:43:32 -0700

    Add edgestore and image dropzone

--- convex/documents.ts
+++ convex/documents.ts
@@ -250,7 +250,7 @@ export const update = mutation({
         id: v.id("documents"),
         title: v.optional(v.string()),
         content: v.optional(v.string()),
-        converImage: v.optional(v.string()),
+        coverImage: v.optional(v.string()),
         icon: v.optional(v.string()),
         isPublished: v.optional(v.boolean())
     },
