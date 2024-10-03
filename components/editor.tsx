9e120735b18b9ca23462319d75fcc1b7724135f6
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-10-03 13:41:09 -0700

    Add blocknote library for document editor

---
+++ components/editor.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+
+import { useTheme } from "next-themes";
+
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  // Allow file upload?
+  //   const handleUpload = async (file: File) => {
+  //     const response = await edgestore.publicFiles.upload({ file });
+  //     return response.url;
+  //   };
+
+  // Add onChange and initialContent to perpetuate content in storage
+
+  const editor = useCreateBlockNote();
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
+};
