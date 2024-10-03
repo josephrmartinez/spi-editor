b270cdfa79439f7d1ce57e284b87bf3280351812
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-10-03 10:28:57 -0700

    Update cover hooks with error handling

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -37,7 +37,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   return (
     <>
-      <nav className="bg-background dark:bg-[#1F1F1F] px-y py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-y py-2 w-full flex items-center gap-x-2">
         {isCollapsed && (
           <MenuIcon
             role="button"
